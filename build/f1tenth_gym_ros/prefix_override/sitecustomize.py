cbec32e143eb2bec9fa0122fc42fc96bba498c1a
TripanDham <tripan.dham@gmail.com>
2025-01-15 16:07:00 +0530

    gym

--- build/f1tenth_gym_ros/prefix_override/sitecustomize.py
+++ build/f1tenth_gym_ros/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/tripan/f1tenthsim_ws/install/f1tenth_gym_ros'
+    sys.prefix = sys.exec_prefix = '/home/tripan/f1tenthsim_ws/f1tenth/install/f1tenth_gym_ros'
--- build/stt_control/prefix_override/sitecustomize.py
+++ build/stt_control/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/tripan/f1tenthsim_ws/install/stt_control'
+    sys.prefix = sys.exec_prefix = '/home/tripan/f1tenthsim_ws/f1tenth/install/stt_control'
